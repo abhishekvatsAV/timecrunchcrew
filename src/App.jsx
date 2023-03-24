9e1a746a54c16edcfa27e1aad844fcc267d7ef83
Guptaverse <sggreat88@gmail.com>
2023-03-24 15:14:34 +0530

    update (timer)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CustomiseRoom from "./pages/CustomiseRoom";
+import UserFirstPage from "./pages/UserFirstPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/customise-room" element={<CustomiseRoom/>} />
+          <Route path="/user" element = {<UserFirstPage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/UserFirstPage.jsx
+++ src/pages/UserFirstPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Clock from '../components/Clock'
+import "./User.css";
 
-const UserFirstPage = () => {
+const UserFirstPage = (heading) => {
   return (
 
     <>
-    <Clock></Clock>
+    {/* <div>{heading}</div> */}
+    <div className="parent">
+        <div className="heading">Schedule Starts in..</div>
+        <div style={{fontSize:"100px"}}>
+        <Clock></Clock>
+        </div>
+    </div>
     </>
   )
 }
