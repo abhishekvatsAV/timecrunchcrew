7c219c611d7e6cd09ca8e0f8b4d51dae04f90688
Abhishek Vats <virus@Abhisheks-MacBook-Air.local>
2023-03-24 14:11:45 +0530

    version-1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import { Parallax } from "react-parallax";
 import Library from "../assets/library.jpg";
 import Library2 from "../assets/library2.jpg";
+import { Button } from "antd";
 
 import "./Home.css";
 
 const Home = () => {
-  const handleClick = () => {
+  const handleCreate = () => {
     // handle create Room
   };
+
+  const handleJoin = () => {
+    // handle room joining
+  };
+
   return (
     <div>
       <Parallax strength={600} bgImage={Library}>
@@ -17,16 +23,29 @@ const Home = () => {
               <h2>Create Room</h2>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-                voluptatem laudantium placeat sed, voluptas obcaecati! 
+                voluptatem laudantium placeat sed, voluptas obcaecati!
               </p>
-              <button onClick={handleClick}>Create Room</button>
+              <Button type="primary" onClick={handleCreate}>
+                Create Room
+              </Button>
             </div>
           </div>
         </div>
       </Parallax>
       <Parallax strength={300} blur={{ min: -15, max: 15 }} bgImage={Library2}>
         <div className="content">
-          <div className="text-content">Normal Parallax</div>
+          <div className="text-content">
+            <div className="joinRoom">
+              <h2>Join Room</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+                voluptatem laudantium placeat sed, voluptas obcaecati!
+              </p>
+              <Button type="primary" onClick={handleClick}>
+                Join Room
+              </Button>
+            </div>
+          </div>
         </div>
       </Parallax>
     </div>
